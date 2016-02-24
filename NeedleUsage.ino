be6a32248f711cccf2528e12bed5f7982dff9d16
CountParadox <cp@tbof.net>
2016-02-25 09:46:15 +1100

    Update NeedleUsage.ino
    -removed old commenting, original code thanks very much to: http://spacetinkerer.blogspot.com <3 for their motor control over serial port code.
    -adjusted max write value to 225 to match the end of the needles range.

--- NeedleUsage.ino
+++ NeedleUsage.ino
@@ -1,17 +1,6 @@
-/*
- * serial_motor_control.pde
- * -----------------
- * Controls the Meter of a motor depending on what is received 
- * through the serial port.
- *
- * http://spacetinkerer.blogspot.com
- */
-
-
 int input = 0;
 
 void setup() {              
-  // initialize the PWM pin #3 as an output.
   pinMode(3, OUTPUT);
   Serial.begin(9600); 
 }
@@ -20,23 +9,18 @@ void loop() {
     input = Serial.read();
     if (input == '0'){
       analogWrite(3, 0);
-      Serial.println("Meter is 0%");
     }
     if (input == '1'){
       analogWrite(3, 64);
-      Serial.println("Meter is at 25%");
     }
     if (input == '2'){
       analogWrite(3, 127);
-      Serial.println("Meter is at 50%");
     }
     if (input == '3'){
       analogWrite(3, 192);
-      Serial.println("Meter is at 75%");
     }
     if (input == '4'){
-      analogWrite(3,255);
-      Serial.println("Meter is at 100%");
+      analogWrite(3,225);
     }
 }
 
